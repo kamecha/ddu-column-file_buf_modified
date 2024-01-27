f837f4a7c1fcbf045d0fe69e2db8f604e0821b91
kamecha <masakikameyama81@gmail.com>
2024-01-27 13:15:57 +0900

    一旦直書きでcolumnを実装

--- denops/@ddu-columns/file_buf_modified.ts
+++ denops/@ddu-columns/file_buf_modified.ts
@@ -3,18 +3,45 @@ import {
   GetTextArguments,
   GetTextResult,
 } from "https://deno.land/x/ddu_vim@v3.4.3/base/column.ts";
+import { fn } from "https://deno.land/x/ddu_vim@v3.4.3/deps.ts";
 import { BaseColumn } from "https://deno.land/x/ddu_vim@v3.4.3/types.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
 
 export type Params = Record<never, never>;
 
 export class Column extends BaseColumn<Params> {
-  getLength({}: GetLengthArguments<Params>): Promise<number> {
-    throw new Error("Method not implemented.");
+  #bufinfos: fn.BufInfo[] = [];
+  async getLength(args: GetLengthArguments<Params>): Promise<number> {
+    this.#bufinfos = await fn.getbufinfo(args.denops, { bufmodified: true });
+    return 1;
   }
-  getText({}: GetTextArguments<Params>): Promise<GetTextResult> {
-    throw new Error("Method not implemented.");
+  getText(args: GetTextArguments<Params>): Promise<GetTextResult> {
+    const action = args.item.action as ActionData;
+    const path = action.path;
+    if (path === undefined) {
+      return Promise.resolve({ text: " " });
+    }
+    if (this.isBufferRelatedFileModified(this.#bufinfos, path)) {
+      return Promise.resolve({ text: "●" });
+    } else {
+      return Promise.resolve({ text: " " });
+    }
   }
   params(): Params {
-    throw new Error("Method not implemented.");
+    return {};
+  }
+  isBufferRelatedFileModified(
+    buffers: fn.BufInfo[],
+    filePath: string,
+  ): boolean {
+    for (const buffer of buffers) {
+      if (buffer.name === undefined) {
+        continue;
+      }
+      if (buffer.name === filePath) {
+        return true;
+      }
+    }
+    return false;
   }
 }
